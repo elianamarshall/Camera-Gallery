c4ae0c0410813f6f85975d2fb8db83016082631d
Eliana Marshall <elianagmarshall@gmail.com>
2026-03-12 22:20:03 -0400

    Rotate camera

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 3.25;
+camera.position.y = 3;
+camera.rotation.x = -0.4;
+camera.rotation.y = 0.01;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
